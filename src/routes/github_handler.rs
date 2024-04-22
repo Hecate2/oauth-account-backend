3136eac52c8e3b074d124ba292a83bad6035a334
Hecate2 <2474101468@qq.com>
2024-04-22 17:27:28 +0800

    json error message

--- src/routes/github_handler.rs
+++ src/routes/github_handler.rs
@@ -1,18 +1,24 @@
 use actix_web::{get, post, web, HttpRequest, HttpResponse, Responder};
 use sea_orm::{ConnectionTrait, Statement};
 use crate::utils::{api_response, app_state::{self, AppState}};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct ErrMessage {
+    err: String,
+}
 
 #[get("/")]
 pub async fn greet(req: HttpRequest) -> impl Responder {
     let authen_header = match req.headers().get("Authorization") {
         Some(authen_header) => authen_header,
         None => {
-            return HttpResponse::Unauthorized().body("No auth")
+            return HttpResponse::Unauthorized().content_type("application/json").json(ErrMessage{err: "No auth".to_string()});
         }
     };
     let authen_str = authen_header.to_str().unwrap_or("");
     if !authen_str.starts_with("bearer") && !authen_str.starts_with("Bearer") {
-        return HttpResponse::Unauthorized().body("Malformed auth")
+        return HttpResponse::Unauthorized().content_type("application/json").json(ErrMessage{err: "Malformed auth".to_string()});
     }
     let raw_token = authen_str[6..authen_str.len()].trim();
     // TODO: call github API with the token and get user id
