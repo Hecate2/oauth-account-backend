1336c4757131b55a519ee7475d8c7aeae888bcf1
Hecate2 <2474101468@qq.com>
2024-05-08 15:44:46 +0800

    refactor

--- src/crypto/bitcoin_keypair.rs
+++ src/crypto/bitcoin_keypair.rs
@@ -1,4 +1,6 @@
+use std::error::Error;
 use rand::rngs::OsRng;
+use secp256k1;
 use secp256k1::Secp256k1;
 use secp256k1::hashes::{sha256::Hash as Sha256Hash, ripemd160::Hash as Ripemp160Hash, Hash};
 
@@ -15,6 +17,25 @@ impl BitcoinKeypair {
     pub fn new() -> BitcoinKeypair {
         let secp = Secp256k1::new();
         let (secret_key, public_key) = secp.generate_keypair(&mut OsRng);
+        return BitcoinKeypair::from_keypair(secret_key, public_key);
+    }
+
+    // checksum not checked!
+    pub fn from_wif(wif: String) -> Result<BitcoinKeypair, Box<dyn Error>> {
+        let mut secret_key_bytes: [u8; 38] = [0x00; 38];
+        bs58::decode(wif).onto(&mut secret_key_bytes)?;
+        Ok(BitcoinKeypair::from_secret_key_slice(&secret_key_bytes[1..33])?)
+    }
+
+    pub fn from_secret_key_slice(s: &[u8]) -> Result<BitcoinKeypair, Box<dyn Error>> {
+        let secp = Secp256k1::new();
+        let sk = secp256k1::SecretKey::from_slice(s)?;
+        let keypair = secp256k1::Keypair::from_secret_key(&secp, &sk);
+        let (secret_key, public_key) = (keypair.secret_key(), keypair.public_key());
+        Ok(BitcoinKeypair::from_keypair(secret_key, public_key))
+    }
+
+    pub fn from_keypair(secret_key: secp256k1::SecretKey, public_key: secp256k1::PublicKey) -> BitcoinKeypair {
         let public_key_bytes = public_key.serialize();
         let public_key_string: String = public_key_bytes.iter()
             .map(|b| format!("{:02X}", b))
